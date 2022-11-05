dc7a9b2c8506c135feafe5970e31ae3b1417190d
HuuThongg <lehuuthong0506@gmail.com>
2022-11-04 23:34:17 -0700

    added all files

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Cart from "./components/Cart/Cart";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+
+
+function App() {
+  return (
+    <>
+      <Cart></Cart>
+      <Header></Header>
+      <main>
+        <Meals></Meals>
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/UI/Modal.js
@@ -0,0 +1,33 @@
+import React from "react";
+import  ReactDOM  from "react-dom";
+
+import classes from './Modal.module.css'
+
+const Backdrop = props =>{
+  return <div className={classes.backdrop}></div>
+}
+const ModalOverlay = props =>{
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  )
+}
+
+const portalElement = document.getElementById('overlays')
+const Modal = props =>{
+
+  return (
+    <>
+      {/* <Backdrop></Backdrop>
+      <ModalOverlay>{props.children}</ModalOverlay> */}
+      {ReactDOM.createPortal(<Backdrop></Backdrop>, portalElement)}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </>
+  )
+  
+}
+export default Modal;
